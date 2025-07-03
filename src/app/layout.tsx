f6bd4064c8ed152600e56d58a4df83f0837a7670
Tai Cameron <tadc936@gmail.com>
2025-07-03 16:05:41 +0100

    Final hopefully3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Script from "next/script";
 
-export default function DashboardLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
@@ -10,17 +10,16 @@ export default function DashboardLayout({
     <html lang="en">
       <body>
         <main>
-          {" "}
           {children}
           <Script id="clarity-script" strategy="afterInteractive">
             {`
-                (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "s962uca1i1");
-          `}
-          </Script>{" "}
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "s962uca1i1");
+            `}
+          </Script>
         </main>
       </body>
     </html>
